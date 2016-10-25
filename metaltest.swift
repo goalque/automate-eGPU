aca74c129743f1fcd2aaa8ef70afce2314b157f5
Delton Ding <dsh0416@gmail.com>
2016-10-25 23:14:21 +0800

    Fix metaltest

--- metaltest.swift
+++ metaltest.swift
@@ -5,7 +5,7 @@ var supportedDevices: [MTLDevice] = MTLCopyAllDevices()
 if (supportedDevices.count > 0)
 {
     for device in supportedDevices {
-        var isSupported = device.supportsFeatureSet(MTLFeatureSet.OSX_GPUFamily1_v1)
+        var isSupported = device.supportsFeatureSet(MTLFeatureSet.osx_GPUFamily1_v1)
         print("\(device.name), supported: \(isSupported)")
     }
 }
